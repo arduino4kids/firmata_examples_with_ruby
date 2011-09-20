5028647e5c092a719bd666d6b3cbdc6209efe633
Alberto Morales <amorales@opnet.com>
2011-09-19 20:23:33 -0400

    explaining proof of concept

--- run.rb
+++ run.rb
@@ -7,8 +7,16 @@ require 'servo'
 
 arduino = Arduino.new(Dir["/dev/tty.usbserial*"][0], 57600)
 
+#we are using the example servo pinout, plugin the servo to pin 9
+#refer to the sweep example in arduino website
 servo = Servo.new(arduino, 9)
 
+#servo.position = 180
+#servo.draw  ##watch the servo move to that position
+#
+#servo.position = 1
+#servo.draw ## watch the servo move back to position 1
+#
 debugger
 
 arduino.quit
